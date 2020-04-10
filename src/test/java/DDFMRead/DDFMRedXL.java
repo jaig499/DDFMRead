26001d0a1b878cffb26f360f17f2a42c01e6076b
jaig499 <jaig499@gmail.com>
2020-04-10 18:17:29 +0530

    add all

---
+++ src/test/java/DDFMRead/DDFMRedXL.java
@@ -0,0 +1,23 @@
+package DDFMRead;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DDFMRedXL {
+	
+	
+		
+		
+		
+		
+		
+	}
+	
+
+
---
+++ src/test/java/log/junit/Junitt.java
@@ -0,0 +1,18 @@
+package log.junit;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Junitt {
+	
+	@Test
+	public void tc1() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Jai\\eclipse-workspace\\Sample\\driver\\chromedriver.exe");
+		
+		
+		
+	}
+
+}
